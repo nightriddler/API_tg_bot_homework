2ffd1dd744c4366eda0419e272fce2e4a9cf621d
Michail Eisenberg <75097575+nightriddler@users.noreply.github.com>
2021-10-11 17:33:34 +0300

    bug fix

--- homework.py
+++ homework.py
@@ -12,7 +12,7 @@ from requests.exceptions import RequestException
 
 load_dotenv()
 
-PRAKTIKUM_TOKEN = os.getenv("PRAKTIKUM_TOKEN")
+PRAKTIKUM_TOKEN = os.getenv('PRAKTIKUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
